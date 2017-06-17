f0d356585eb33425dde68665f254d81b445f86d5
prokarius <rollingwithoutslipping@yahoo.com>
2017-06-17 23:09:48 +0800

    th

--- chatbot.py
+++ chatbot.py
@@ -15,39 +15,51 @@ app = Flask(__name__)
 
 ACCESS_TOKEN = "EAAXZBh5wBB74BACCysn5riSQsjmPt1igZCZCopKWzTW0InXultylLBnbvozZBwpOIu75JMGk62sZAgvyjtp2KHYJ5ezZCrWovgdGdP4zaTcigJEORYJiR0JYlSdefXgCrN2lcsS6BFMx70WlyOShS6aC6YP345nFtpbRgRkeMY5gZDZD" 
 
-#The API call for buttons for after you say Hi to Rize. <3
-startthing = {
-  "recipient":{
-    "id":"USER_ID"
-  },
-  "message":{
-    "text":"Pick a color:",
-    "quick_replies":[
-      {
-        "content_type":"text",
-        "title":"Tips",
-        "payload":"Tips"
-      },
-      {
-        "content_type":"text",
-        "title":"Recycling Help!",
-        "payload":"Recycling Help"
-      }
-    ]
-  }
-}
-
 def get_maps():
 	'''Return map image of location'''
 	return requests.get("https://image.maps.cit.api.here.com/mia/1.6/mapview?c=" +latitude+"%2"+longitude+"&z=14"+ "&app_id="+app_id+"&app_code="+app_code)
 
 
+def quick_reply_API(user_id,list_of_text, displaytext):
+	"""
+	Sends the API call to generate the button.
+	"""
+	#The API call for buttons. Takes a list of text to display. Generates the JSON code to submit as the API call. Display text is what the chat bot will reply to the user. Generally limit list of text to at more 4 strings. Display text can be capitalised.
+	startthing = {
+		"recipient":{
+	    "id":user_id
+		},
+		"message":{
+    		"text": displaytext,
+    		"quick_replies":[]
+		}
+	}
+	for i in list_of_text:
+		phdict = {}
+		phdict ["content_type"] = "text"
+		phdict ["title"] = i
+		phdict ["payload"] = "placeholder"	
+		startthing["message"]["quick_replies"].append(phdict)
+
+	resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=startthing)
+
+
 def reply_user(user_id,msg):
-	data = {
-	      "recipient": {"id": user_id},
-	      "message": {"text": msg}
-					    }
+	# Function to Reply the user. Checks for case of API calls beforehand.	
+	flag = -1
+	msg = msg[0].upper() + msg[1:]
+	if "||s" in msg:
+		flag = 1
+		msg = msg[:-4]
+	elif "||d" in msg:
+		flag = 2
+		msg = msg[:-4]
+	data = {"recipient": {"id": user_id}, "message": {"text": msg}}
 	resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+	if flag == 1:
+		
+		quick_reply_API(user_id, ["Recycle", "Daily Tips", "Events"], "Select what you would like help with.")
+	
 	print(resp.content)
 
 @app.route('/', methods=['GET'])
