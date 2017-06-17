2f35d5c2a93f9ff69d5f2d57ca48dafac5c0730c
prokarius <rollingwithoutslipping@yahoo.com>
2017-06-17 21:48:13 +0800

    Added some stuff

--- chatbot.py
+++ chatbot.py
@@ -5,18 +5,42 @@ from rivescript import RiveScript
 bot = RiveScript()
 bot.load_directory("./eg/brain")
 bot.sort_replies()
-
+#Relevant Parameters for HERE Technologies
+app_id=123
+app_code=1
+latitude=2
+longitude=3
+#Relevant parameters for HERE technologies
 app = Flask(__name__)
 
 ACCESS_TOKEN = "EAAXZBh5wBB74BACCysn5riSQsjmPt1igZCZCopKWzTW0InXultylLBnbvozZBwpOIu75JMGk62sZAgvyjtp2KHYJ5ezZCrWovgdGdP4zaTcigJEORYJiR0JYlSdefXgCrN2lcsS6BFMx70WlyOShS6aC6YP345nFtpbRgRkeMY5gZDZD" 
 
-def verify_hash (user_id,userinput):
-	try:
-		score = int(userinput)
-		message = "You have earned " + userinput + " points today!"
-	except (ValueError):
-		message = "Please Type in a Valid Key"	
-	reply_user(user_id,message)
+#The API call for buttons for after you say Hi to Rize. <3
+startthing = {
+  "recipient":{
+    "id":"USER_ID"
+  },
+  "message":{
+    "text":"Pick a color:",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Tips",
+        "payload":"Tips"
+      },
+      {
+        "content_type":"text",
+        "title":"Recycling Help!",
+        "payload":"Recycling Help"
+      }
+    ]
+  }
+}
+
+def get_maps():
+	'''Return map image of location'''
+	return requests.get("https://image.maps.cit.api.here.com/mia/1.6/mapview?c=" +latitude+"%2"+longitude+"&z=14"+ "&app_id="+app_id+"&app_code="+app_code)
+
 
 def reply_user(user_id,msg):
 	data = {
